df388f5c3fed66a003a6d7d45c4b8e116828f65f
jdcar <jdcarlstone@gmail.com>
2021-11-24 09:52:18 -0600

    Classification number updates sandbox

--- index.js
+++ index.js
@@ -1,83 +1,45 @@
 // const fetch = require('node-fetch')
-const axios = require('axios').default;
+const axios = require('axios').default
 const config = require('./config.js')
-const productionKey = config.PRODUCTION_API_KEY;
-const sandboxKey = config.SANDBOX_API_KEY;
+const productionKey = config.PRODUCTION_API_KEY
+const sandboxKey = config.SANDBOX_API_KEY
 
-// The bib record needs to be in MARC xml. You also need to wrap it in a <bib> tag and delete the header "<?xml version="1.0" encoding="UTF-16"?>"
-// The XML sample at the bottom of the page here https://developers.exlibrisgroup.com/alma/apis/docs/xsd/rest_bib.xsd/?tags=PUT is the formatting to follow. The XML can be all on 1 line.
+let csv = require('fast-csv')
+const fs = require('fs')
 
-//Replace MARCXML_BIB_RECORD with the updated bib in MARCXML. You have to update the entire bib record while doing a PUT request.
-let marc = 'MARCXML_BIB_RECORD'
-
-// Replace MMS ID with the id from your system
-let mmsid = '9953136504202441'
-
-let holding_id = "22512378340002441"
-
-let xmlRecord = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<holding>
-    <holding_id>22512378340002441</holding_id>
-    <created_by>import</created_by>
-    <created_date>2015-07-09Z</created_date>
-    <last_modified_by>exl_api</last_modified_by>
-    <last_modified_date>2021-11-23Z</last_modified_date>
-    <originating_system>ILS</originating_system>
-    <originating_system_id>5593056-nwudb</originating_system_id>
-    <suppress_from_publishing>false</suppress_from_publishing>
-    <calculated_suppress_from_publishing>false</calculated_suppress_from_publishing>
-    <record>
-        <leader>00175cx  a22000854  4500</leader>
-        <controlfield tag="001">5593056</controlfield>
-        <controlfield tag="004">5313650</controlfield>
-        <controlfield tag="005">20211123121118.0</controlfield>
-        <controlfield tag="008">0903314u    8   1001uu   0901128</controlfield>
-        <datafield ind1="0" ind2=" " tag="852">
-            <subfield code="b">UNTD</subfield>
-            <subfield code="c">ug</subfield>
-            <subfield code="h">TEST</subfield>
-            <subfield code="i">S4</subfield>
-        </datafield>
-    </record>
-</holding>`
-
-
-// console.log(marc)
-
-var headerConfig = {
+const headerConfig = {
     headers: {
         'Content-Type': 'application/xml'
 
     }
-};
+}
 
-let url = `https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/${mmsid}/holdings/${holding_id}?apikey=${sandboxKey}`
+var stream = fs.createReadStream("data.csv");
 
-// //Switch to the production key in your config.js file by changing sandboxKey to productionKey
+csv
+    .parseStream(stream, { headers: true })
+    .on("data", function (data) {
+        //  console.log(data.MMSID);
 
-axios.put(`https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/${mmsid}/holdings/${holding_id}?apikey=${sandboxKey}`, xmlRecord, headerConfig)
-    .then(res => {
-        console.log(res)
+        axios.get(`https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/${data.MMSID}/holdings/${data.HoldingId}?apikey=${sandboxKey}`)
+            .then(res => {
+
+                const xml = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?><holding><holding_id>${res.data.holding_id}</holding_id><created_by>${res.data.created_by}</created_by><created_date>${res.data.created_date}</created_date><last_modified_by>${res.data.last_modified_by}</last_modified_by><last_modified_date>${res.data.last_modified_date}</last_modified_date><originating_system>${res.data.originating_system}</originating_system><originating_system_id>${res.data.originating_system_id}</originating_system_id><suppress_from_publishing>${res.data.suppress_from_publishing}</suppress_from_publishing><calculated_suppress_from_publishing>${res.data.calculated_suppress_from_publishing}</calculated_suppress_from_publishing>${res.data.anies.toString().replace(/\n/gm, "").replace(/(<subfield code="h">)(.+?)(<\/subfield>)/, `$1${data.Classification}$3`).replace(/<\?xml version.+?>/gm, "")}</holding>`
+
+                axios.put(`https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/${data.MMSID}/holdings/${data.HoldingId}?apikey=${sandboxKey}`, xml, headerConfig)
+                    .then(res => {
+                        console.log(res)
+                    })
+                    .catch(error => {
+                        console.log(error)
+                    })
+
+            }).catch(error => {
+                console.log(error)
+            })
+
     })
-    .catch(error => {
-        console.log(error)
-    })
+    .on("end", function () {
+        console.log("done")
+    });
 
-
-
-
-
-axios.get(url)
-    .then(res => {
-
-        // console.log(res.data.anies.toString().replace(/\n/g, ""))
-
-        // let newRecord = res.data.anies.toString().replace(/\n|\t/g, "").replace(/>\s+</g, "><").replace(/(<subfield code=\"h\">)(BF432.N5)(<\/subfield>)/g, "$1BF432.B53$3")
-
-        console.log(res.data.anies[0])
-
-        // putRequest(newRecord)
-
-    }).catch(error => {
-        console.log(error)
-    })
