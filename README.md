7c600322c9e7f267beac88e20c87e542088121c4
jdcar <jdcarlstone@gmail.com>
2021-12-10 11:07:40 -0600

    Refractor as asynch await and error handling

--- README.md
+++ README.md
@@ -10,16 +10,20 @@ This is built specifically for updating cutter numbers after the change Library 
 ## Installation Instructions
 First, you will need an Alma API key with read/write permissions from your institution. In the alma-call-number-update main repo folder, create a 'config.js' file. Add this to the file with your API keys in quotes:
 ```
-//The sandbox key is for testing, you will need to edit the code to change SANDBOX_API_KEY to PRODUCTION_API_KEY when ready to update records.
+//The sandbox key is for testing, you will need to edit the index.js file to change SANDBOX_API_KEY to PRODUCTION_API_KEY when ready to update records.
 module.exports = config = {
     PRODUCTION_API_KEY : 'ENTER_PRODUCTION_KEY_HERE',
     SANDBOX_API_KEY : 'ENTER_SANDBOX_KEY_HERE',
   }
   ```
 
-Second, you'll need a csv of the new classification number*, the holdings ID and the MMSID. Add the csv file to the alma-call-number-update main repo folder and call it 'data.csv' using the format in 'example-data.csv.' Make sure the column headers match exactly.
+Second, you'll need a csv of the new classification number*, the holdings ID and the MMSID. Add the csv file to the alma-call-number-update main repo folder and call it 'data.csv' using the format in 'example-data.csv.' Make sure the column headers match exactly. *Note: the file is csv, but the data in the file is tab delimited to simplify a copy-paste from a spreadsheet*
 
 *The second column should be the new 852 subfield $h only.
+
+When testing this on a file of 650 records I kept running into an error where I was making too many API calls/second. I refractored to make the code asynchronous but the issue continued. The workaround I found was only doing 20 records at a time.
+
+If there is an error with a record it will write to a file with the MMSIDs that didn't GET or PUT.
 
 Install dependencies
 ```
--- index.js
+++ index.js
@@ -14,32 +14,53 @@ const headerConfig = {
     }
 }
 
+
 var stream = fs.createReadStream("data.csv");
 
-csv
-    .parseStream(stream, { headers: true })
+csv.parseStream(stream, { headers: true, delimiter: '\t' })
     .on("data", function (data) {
         //  console.log(data.MMSID);
-
-        axios.get(`https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/${data.MMSID}/holdings/${data.HoldingId}?apikey=${sandboxKey}`)
-            .then(res => {
+        const sendGetRequest = async () => {
+            try {
+                const res = await axios.get(`https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/${data.MMSID}/holdings/${data.HoldingId}?apikey=${productionKey}`);
 
                 const xml = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?><holding><holding_id>${res.data.holding_id}</holding_id><created_by>${res.data.created_by}</created_by><created_date>${res.data.created_date}</created_date><last_modified_by>${res.data.last_modified_by}</last_modified_by><last_modified_date>${res.data.last_modified_date}</last_modified_date><originating_system>${res.data.originating_system}</originating_system><originating_system_id>${res.data.originating_system_id}</originating_system_id><suppress_from_publishing>${res.data.suppress_from_publishing}</suppress_from_publishing><calculated_suppress_from_publishing>${res.data.calculated_suppress_from_publishing}</calculated_suppress_from_publishing>${res.data.anies.toString().replace(/\n/gm, "").replace(/(<subfield code="h">)(.+?)(<\/subfield>)/, `$1${data.Classification}$3`).replace(/<\?xml version.+?>/gm, "")}</holding>`
 
-                axios.put(`https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/${data.MMSID}/holdings/${data.HoldingId}?apikey=${sandboxKey}`, xml, headerConfig)
-                    .then(res => {
-                        console.log(res)
-                    })
-                    .catch(error => {
-                        console.log(error)
-                    })
 
-            }).catch(error => {
-                console.log(error)
-            })
+                const sendPutRequest = async () => {
+                    try {
+                        const resp = await axios.put(`https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/${data.MMSID}/holdings/${data.HoldingId}?apikey=${productionKey}`, xml, headerConfig);
+                        console.log(resp.data);
+                     
+                    } catch (err) {
+                        // Handle Error Here
+                        fs.appendFile('putError.txt', data.MMSID.toString(), function (err) {
+                            if (err) throw err;
+                        })
+                    }
+                };
+
+                sendPutRequest()
+             
+
+            } catch (err) {
+                // Handle Error Here
+                fs.appendFile('getError.txt', data.MMSID.toString(), function (err) {
+                    if (err) throw err;
+    
+                })
+            }
+        };
+
+        sendGetRequest()
+
+
 
     })
     .on("end", function () {
         console.log("done")
     });
+
+
+
 
